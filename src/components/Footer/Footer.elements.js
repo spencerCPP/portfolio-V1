81f48f3b562f5e00694a35733611d6e5f6a8bf18
spencerCPP <srbarrett@cpp.edu>
2022-04-04 01:35:13 -0700

    Created footer component and styling

---
+++ src/components/Footer/Footer.elements.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  background-color: #0f0f0f;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
+
+export const FooterText = styled.div`
+  height: 6rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-family: "Work Sans", sans-serif;
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FooterContainer, FooterText } from './Footer.elements'
+
+const Footer = () =>{
+    return(
+        <>
+        <FooterContainer>
+        <FooterText>
+            Yo
+            </FooterText>
+        </FooterContainer>
+        </>
+    );
+}
+
+export default Footer;
