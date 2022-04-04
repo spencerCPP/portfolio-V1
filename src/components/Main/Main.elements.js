e0164812e8781b1c4d3b592b1ed6d827f4ca01f8
spencerCPP <srbarrett@cpp.edu>
2022-04-04 01:36:35 -0700

    Initialized compenents/styling

---
+++ src/components/Main/Main.elements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+box-sizing: border-box;
+height: 100%;
+background-color: #0f0f0f;
+color: #fff;
+
+`
---
+++ src/components/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MainContainer } from './Main.elements';
+const Main = () => {
+    return(
+        <>
+            <MainContainer>
+                <div>
+                    middle
+                </div>
+            </MainContainer>
+        </>
+
+    );
+}
+
+export default Main;
