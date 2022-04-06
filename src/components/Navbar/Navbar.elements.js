32b6eeb2f2425bd6ce3eed6c474344aa71d7a728
spencerCPP <srbarrett@cpp.edu>
2022-04-06 11:52:33 -0700

    Finished implementation of Navbar

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -3,26 +3,23 @@ import { Link as LinkR } from "react-router-dom";
 
 export const Nav = styled.nav`
   background: #0f0f0f;
-  height: 4rem;
-  z-index: 10;
-  position: sticky;
-
-  @media screen and (max-width: 960px) {
-    transition: 0.8s all ease;
-  }
+  min-height: 4rem;
+  z-index: 999;
+  position: fixed;
+  min-width: 100%;
 `;
 export const NavbarContainer = styled.div`
-  height: 4rem;
+  min-height: 4rem;
   z-index: 1;
   width: 100%;
   display: flex;
+  
 `;
 
 export const NavbarBrandContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 10px;
   padding-left: 2px;
 `;
 
@@ -33,6 +30,16 @@ export const NavbarLogo = styled(LinkR)`
   cursor: pointer;
   margin-left: 10px;
   text-decoration: none;
+  img {
+    width: 45px;
+    height: 45px;
+  }
+  ${'' /* @media screen and (max-width: 769px) {
+    img {
+      width: 35px;
+      height: 35px;
+    }
+  } */}
 `;
 
 export const NavbarLogoText = styled(LinkR)`
@@ -47,4 +54,62 @@ export const NavbarLogoText = styled(LinkR)`
   padding-bottom: 4px;
   padding-left: 7px;
   opacity: 0.6;
+`;
+
+export const LinkWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+    margin-left: auto;
+  
+`;
+
+export const LinkContainer = styled.ul`
+
+margin: 1.25rem;
+  
+
+  @media screen and (max-width: 769px) {
+    display: none;
+  }
+`;
+
+export const NavbarLinkItem = styled.li`
+float: left;
+  margin-left: 1.25rem;
+  list-style: none;
+ 
+`;
+
+export const NavbarLink = styled(LinkR)`
+  color: #a5a5a5;
+  opacity: 0.75;
+  font-family: "Lato", sans-serif;
+  text-decoration: none;
+  :hover {
+    opacity: 1;
+  }
+`;
+
+export const NavbarCollapse = styled.div`
+
+`
+
+export const NavbarCollapseLogo = styled.div`
+  display: none;
+
+  @media screen and (max-width: 769px) {
+    color: #a5a5a5;
+    opacity: 0.75;
+    cursor: pointer;
+    border: 1px solid #a5a5a5;
+    border-radius: 10%;
+    :hover {
+      opacity: 1;
+    }
+    display: flex;
+    position: absolute;
+    top: 20px;
+    right: 10px;
+  }
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,26 +5,46 @@ import {
   NavbarLogoText,
   NavbarLogo,
   NavbarBrandContainer,
+  NavbarLink,
+  LinkWrapper,
+  LinkContainer,
+  NavbarLinkItem,
+  NavbarCollapseLogo
 } from "./Navbar.elements";
 import SpiderLogo from "./BrandLogo/SpiderIcon.svg";
+import { GoThreeBars } from 'react-icons/go'
 
-const Navbar = () => {
+const Navbar = ({ isOpen, toggle }) => {
   return (
     <>
       <Nav>
-        <NavbarContainer>
-          <NavbarBrandContainer>
+        <NavbarContainer >
+          <NavbarBrandContainer >
             <NavbarLogo to="/">
               <img
                 alt=""
                 src={SpiderLogo}
-                width="45"
-                height="45"
                 className="d-inline-block align-top"
               />{" "}
             </NavbarLogo>
             <NavbarLogoText to="/">SPENCER BARRETT</NavbarLogoText>
           </NavbarBrandContainer>
+          <NavbarCollapseLogo onClick={toggle}>
+            <GoThreeBars size={20}/>
+          </NavbarCollapseLogo>
+          <LinkWrapper>
+          <LinkContainer>
+            <NavbarLinkItem>
+              <NavbarLink to="/">Home</NavbarLink>
+            </NavbarLinkItem>
+            <NavbarLinkItem>
+              <NavbarLink to="/about">About</NavbarLink>
+            </NavbarLinkItem>
+            <NavbarLinkItem>
+              <NavbarLink to="/contact">Contact</NavbarLink>
+            </NavbarLinkItem>
+          </LinkContainer>
+          </LinkWrapper>
         </NavbarContainer>
       </Nav>
     </>
