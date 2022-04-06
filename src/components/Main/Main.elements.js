5bec14bef64138497943a44bd8dafa4350272374
spencerCPP <srbarrett@cpp.edu>
2022-04-06 11:52:07 -0700

    Temporary implementation of main content

--- src/components/Main/Main.elements.js
+++ src/components/Main/Main.elements.js
@@ -1,9 +1,28 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-box-sizing: border-box;
-height: 100%;
-background-color: #0f0f0f;
-color: #fff;
+justify-content: center;
+align-items: center;
+background-color: #fff;
+background: transparent;
+`
 
+export const MainContent = styled.div`
+top: 4rem;
+left: 0;
+right:0;
+background-color: #000;
+opacity: 0.35;
+margin-top: 0;
+margin-left:40px;
+margin-right: 40px;
+margin-bottom: 40px;
+`
+export const VideoPlayer = styled.video`
+width: 100%;
+height: 100%;
+${'' /* -o-object-fit: cover;
+object-fit: cover; */}
+background: #333;
+z-index: 3;
 `
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { MainContainer } from './Main.elements';
+import React from "react";
+import { MainContainer, MainContent, VideoPlayer } from "./Main.elements";
+import Video from "../Videos/video.mp4";
+import Poster from "../Videos/cover.png";
 const Main = () => {
-    return(
-        <>
-            <MainContainer>
-                <div>
-                    middle
-                </div>
-            </MainContainer>
-        </>
 
-    );
-}
+  return (
+    <>
+      <MainContainer>
+        <MainContent>  
+          <VideoPlayer autoPlay loop muted playsInline src={Video} poster={Poster} type="video/mp4" />
+        </MainContent>
+      </MainContainer>
+    </>
+  );
+};
 
 export default Main;
