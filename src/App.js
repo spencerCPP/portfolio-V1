bad7994d7203542ab723f46ddd52e1ea82fa598b
spencerCPP <srbarrett@cpp.edu>
2022-04-08 23:36:51 -0700

    Re-commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import Layout from './components/Layout';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import Layout from "./components/Layout";
+import Main from "./components/Main";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
-     <Layout />
-    </BrowserRouter>
-    
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
